50561f29196beaa5c26f0f83905a556a2b710cbe
James Ponza <jponza@gmail.com>
2021-11-22 19:13:54 +1100

    Fixed indentation

--- parse.py
+++ parse.py
@@ -15,72 +15,72 @@ out = open('visit_history__short_coords.csv','w')
 out.write("PlaceID,Name,Location,Latitude,Longitude,StartTime,EndTime\n")
 
 for year in years:
-        files = os.listdir("%s/%s" % (basedir,year))
-        files = [ "%s/%s/%s" % (basedir,year,file) for file in files if file.endswith("json") ]
-        for file in files:
-            print(file)
-            f = open(file,)
-            data = json.load(f)
+    files = os.listdir("%s/%s" % (basedir,year))
+    files = [ "%s/%s/%s" % (basedir,year,file) for file in files if file.endswith("json") ]
+    for file in files:
+        print(file)
+        f = open(file,)
+        data = json.load(f)
 
-            objects = data['timelineObjects']
+        objects = data['timelineObjects']
 
-            visits = []
+        visits = []
 
-            for o in objects:
+        for o in objects:
 
-                visit = {}
+            visit = {}
 
-                if 'placeVisit' in o:
-                    v = o['placeVisit']
+            if 'placeVisit' in o:
+                v = o['placeVisit']
 
-                    if 'location' not in v or 'duration' not in v:
-                        next
+                if 'location' not in v or 'duration' not in v:
+                    next
 
-                    l = v['location']
-                    d = v['duration']
+                l = v['location']
+                d = v['duration']
 
-                    for key in 'placeId', 'name','address','latitudeE7','longitudeE7':
+                for key in 'placeId', 'name','address','latitudeE7','longitudeE7':
 
-                        val = ''
+                    val = ''
 
-                        if key in l:
-                            val = l[key]
-                            #print("key='%s' val='%s'" % (key,val))
+                    if key in l:
+                        val = l[key]
+                        #print("key='%s' val='%s'" % (key,val))
 
-                        if key in [ 'name', 'address' ]:
-                            val = val.replace(',',' ')
-                            val = val.replace('\n',' ')
+                    if key in [ 'name', 'address' ]:
+                        val = val.replace(',',' ')
+                        val = val.replace('\n',' ')
 
-                        if key in [ 'latitudeE7', 'longitudeE7' ]:
-                            if val == '':
-                                val = 0
-                            val = int(val)
-                            #print('val=%s' % val)
-                            try:
-                                val /= 10000000.0
-                            except ValueError as e:
-                                print("BAD [ValueError]: key=%s val=%s: %s" % (key,val,l))
+                    if key in [ 'latitudeE7', 'longitudeE7' ]:
+                        if val == '':
+                            val = 0
+                        val = int(val)
+                        #print('val=%s' % val)
+                        try:
+                            val /= 10000000.0
+                        except ValueError as e:
+                            print("BAD [ValueError]: key=%s val=%s: %s" % (key,val,l))
 
-                            # limit to 3
-                            val = "%.03f" % val
-                            #val = "%.06f" % val
+                        # limit to 3
+                        val = "%.03f" % val
+                        #val = "%.06f" % val
 
-                            key = key.replace('E7','')
+                        key = key.replace('E7','')
 
-                        visit[key] = val
+                    visit[key] = val
 
-                    for key in 'startTimestampMs', 'endTimestampMs':
-                        val = d[key]
-                        val = pd.to_datetime( val, unit='ms' )
-                        # remove milliseconds
+                for key in 'startTimestampMs', 'endTimestampMs':
+                    val = d[key]
+                    val = pd.to_datetime( val, unit='ms' )
+                    # remove milliseconds
+                    print("val=%s" % val)
+                    if len(str(val)) == 26:
+                        val = str(val)[:-7]
                         print("val=%s" % val)
-                        if len(str(val)) == 26:
-                            val = str(val)[:-7]
-                            print("val=%s" % val)
-                        visit[key] = val
+                    visit[key] = val
 
-                    visits.append(visit)
+                visits.append(visit)
 
-            for v in visits:
-                out.write("%s,'%s','%s',%s,%s,%s,%s\n" % (v['placeId'], v['name'], v['address'], v['latitude'], v['longitude'], v['startTimestampMs'], v['endTimestampMs']))
+        for v in visits:
+            out.write("%s,'%s','%s',%s,%s,%s,%s\n" % (v['placeId'], v['name'], v['address'], v['latitude'], v['longitude'], v['startTimestampMs'], v['endTimestampMs']))
 
