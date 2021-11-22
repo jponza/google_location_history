6bbad2bfa28a7bb260acde612c422e8a46fd8925
James Ponza <jponza@gmail.com>
2021-11-22 19:07:47 +1100

    Update parse.py

--- parse.py
+++ parse.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import os
 import json
+import pandas as pd
 
 basedir="/home/kali/scripts/googlemaps/Takeout/Location History/Semantic Location History"
 
@@ -8,7 +9,10 @@ years = os.listdir(basedir)
 
 visits = {}
 
-out = open('visit_history.csv','w')
+out = open('visit_history__short_coords.csv','w')
+#out = open('visit_history.csv','w')
+
+out.write("PlaceID,Name,Location,Latitude,Longitude,StartTime,EndTime\n")
 
 for year in years:
         files = os.listdir("%s/%s" % (basedir,year))
@@ -41,19 +45,42 @@ for year in years:
 
                         if key in l:
                             val = l[key]
-                            print("key='%s' val='%s'" % (key,val))
+                            #print("key='%s' val='%s'" % (key,val))
 
                         if key in [ 'name', 'address' ]:
+                            val = val.replace(',',' ')
                             val = val.replace('\n',' ')
+
+                        if key in [ 'latitudeE7', 'longitudeE7' ]:
+                            if val == '':
+                                val = 0
+                            val = int(val)
+                            #print('val=%s' % val)
+                            try:
+                                val /= 10000000.0
+                            except ValueError as e:
+                                print("BAD [ValueError]: key=%s val=%s: %s" % (key,val,l))
+
+                            # limit to 3
+                            val = "%.03f" % val
+                            #val = "%.06f" % val
+
+                            key = key.replace('E7','')
 
                         visit[key] = val
 
                     for key in 'startTimestampMs', 'endTimestampMs':
                         val = d[key]
+                        val = pd.to_datetime( val, unit='ms' )
+                        # remove milliseconds
+                        print("val=%s" % val)
+                        if len(str(val)) == 26:
+                            val = str(val)[:-7]
+                            print("val=%s" % val)
                         visit[key] = val
 
                     visits.append(visit)
 
             for v in visits:
-                out.write("%s,'%s','%s',%s,%s,%s,%s\n" % (v['placeId'], v['name'], v['address'], v['latitudeE7'], v['longitudeE7'], v['startTimestampMs'], v['endTimestampMs']))
+                out.write("%s,'%s','%s',%s,%s,%s,%s\n" % (v['placeId'], v['name'], v['address'], v['latitude'], v['longitude'], v['startTimestampMs'], v['endTimestampMs']))
 
